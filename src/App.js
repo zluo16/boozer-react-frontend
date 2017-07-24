a7fbf0a6d0f58a228f3157619ae0fb71faed6f44
zluo16 <zluo@alumni.berklee.edu>
2017-07-24 15:28:30 -0400

    created a fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    cocktails: []
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:3000/api/v1/cocktails'
+
+    fetch(url)
+      .then(res => res.json())
+      .then(cocktails => {
+        this.setState({
+          cocktails: cocktails
+        })
+      })
+  }
+
   render() {
+    debugger
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
       </div>
     );
   }
